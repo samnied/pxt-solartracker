c5773c83f511431b81651f18b4e2b899f16ea67b
samnied <43746162+samnied@users.noreply.github.com>
2019-10-22 22:36:17 +0200

    add test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,12 @@
-// tests go here; this will not be compiled when this package is used as a library
+basic.forever(function () {
+    if (true) {
+        serial.writeValue("tl", Solar.readLDR(ldrId.TopLeft))
+        serial.writeValue("tr", Solar.readLDR(ldrId.TopRight))
+        serial.writeValue("bl", Solar.readLDR(ldrId.BottomLeft))
+        serial.writeValue("br", Solar.readLDR(ldrId.BottomRight))
+        serial.writeValue("P", Solar.readServo(servoId.Pan))
+        serial.writeValue("T", Solar.readServo(servoId.Tilt))
+        serial.writeValue("SC", Solar.readSolarCell())
+        basic.pause(500)
+    }
+})
